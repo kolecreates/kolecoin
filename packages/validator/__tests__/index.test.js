1b1b40535c0b6a3fe2813d81d953b4c7b4ae86c0
kole <Kfnunley@gmail.com>
2021-07-25 16:28:58 -0400

    more txPoolToBlock tests

--- packages/validator/__tests__/index.test.js
+++ packages/validator/__tests__/index.test.js
@@ -1,5 +1,11 @@
 /* eslint-disable no-undef */
-const { FROM_ROOT_TO_USER_1, FROM_ROOT_TO_USER_2, VERIFIER_WALLET } = require('@kolecoin/core/__mocks__/data');
+const {
+  FROM_ROOT_TO_USER_1,
+  FROM_ROOT_TO_USER_2,
+  VERIFIER_WALLET,
+  USER_WALLET_1,
+  USER_WALLET_2,
+} = require('@kolecoin/core/__mocks__/data');
 const { createLedger } = require('@kolecoin/core/lib/ledger');
 const blockUtils = require('@kolecoin/core/lib/blocks');
 
@@ -8,51 +14,209 @@ const { ROOT_BLOCK, TOTAL_COIN_SUPPLY, BASE_TX_FEE } = require('@kolecoin/core/l
 const { txPoolToBlock } = require('../lib/index');
 
 const nowSpy = jest.spyOn(Date, 'now');
+const id = createBlockIdSpy();
+const timestamp = Date.now();
+const BLOCK_1 = {
+  id,
+  prev: ROOT_BLOCK.id,
+  timestamp,
+  transactions: [
+    FROM_ROOT_TO_USER_1,
+    FROM_ROOT_TO_USER_2,
+  ],
+  lookup: {
+    [FROM_ROOT_TO_USER_1.to]: {
+      balance: FROM_ROOT_TO_USER_1.value,
+    },
+    [FROM_ROOT_TO_USER_2.to]: {
+      balance: FROM_ROOT_TO_USER_2.value,
+    },
+    [FROM_ROOT_TO_USER_1.from]: {
+      nonce: 1,
+      balance: TOTAL_COIN_SUPPLY
+          - FROM_ROOT_TO_USER_1.value
+          - FROM_ROOT_TO_USER_2.value
+          - (BASE_TX_FEE * 2),
+      fees: [
+        BASE_TX_FEE,
+        BASE_TX_FEE,
+      ],
+    },
+  },
+  verifier: VERIFIER_WALLET.publicKey,
+  proofOfAuth: VERIFIER_WALLET.proofOfAuth,
+};
 
 describe('txPoolToBlock', () => {
   it('converts valid pool to block', () => {
-    const id = '123456';
-    const timestamp = Date.now();
     createBlockIdSpy.mockReturnValueOnce(id);
     nowSpy.mockReturnValueOnce(timestamp);
-    const pool = [
-      FROM_ROOT_TO_USER_1,
-      FROM_ROOT_TO_USER_2,
-    ];
     expect(
       txPoolToBlock(
         createLedger(),
-        pool,
+        [
+          FROM_ROOT_TO_USER_1,
+          FROM_ROOT_TO_USER_2,
+        ],
+        VERIFIER_WALLET.publicKey,
+        VERIFIER_WALLET.proofOfAuth,
+      ),
+    ).toEqual(
+      BLOCK_1,
+    );
+  });
+
+  it('sorts transactions in block by nonce', () => {
+    createBlockIdSpy.mockReturnValueOnce(id);
+    nowSpy.mockReturnValueOnce(timestamp);
+    expect(
+      txPoolToBlock(
+        createLedger(),
+        [
+          FROM_ROOT_TO_USER_2,
+          FROM_ROOT_TO_USER_1,
+        ],
+        VERIFIER_WALLET.publicKey,
+        VERIFIER_WALLET.proofOfAuth,
+      ),
+    ).toEqual(
+      BLOCK_1,
+    );
+  });
+
+  it('ignores bad nonce transactions', () => {
+    createBlockIdSpy.mockReturnValueOnce(id);
+    nowSpy.mockReturnValueOnce(timestamp);
+    expect(
+      txPoolToBlock(
+        createLedger(),
+        [
+          FROM_ROOT_TO_USER_1,
+          {
+            ...FROM_ROOT_TO_USER_2,
+            nonce: 0,
+          },
+        ],
         VERIFIER_WALLET.publicKey,
         VERIFIER_WALLET.proofOfAuth,
       ),
     ).toEqual(
       {
-        id,
-        prev: ROOT_BLOCK.id,
-        timestamp,
-        transactions: pool,
+        ...BLOCK_1,
+        transactions: [
+          FROM_ROOT_TO_USER_1,
+        ],
         lookup: {
           [FROM_ROOT_TO_USER_1.to]: {
             balance: FROM_ROOT_TO_USER_1.value,
           },
-          [FROM_ROOT_TO_USER_2.to]: {
-            balance: FROM_ROOT_TO_USER_2.value,
-          },
           [FROM_ROOT_TO_USER_1.from]: {
-            nonce: 1,
+            nonce: 0,
             balance: TOTAL_COIN_SUPPLY
                 - FROM_ROOT_TO_USER_1.value
-                - FROM_ROOT_TO_USER_2.value
-                - (BASE_TX_FEE * 2),
+                - (BASE_TX_FEE * 1),
             fees: [
-              BASE_TX_FEE,
               BASE_TX_FEE,
             ],
           },
         },
-        verifier: VERIFIER_WALLET.publicKey,
-        proofOfAuth: VERIFIER_WALLET.proofOfAuth,
+      },
+    );
+  });
+
+  it('ignore bad balance transactions', () => {
+    createBlockIdSpy.mockReturnValueOnce(id);
+    nowSpy.mockReturnValueOnce(timestamp);
+    expect(
+      txPoolToBlock(
+        createLedger(),
+        [
+          FROM_ROOT_TO_USER_1,
+          {
+            from: USER_WALLET_1.publicKey,
+            to: USER_WALLET_2.publicKey,
+            value: 20,
+            nonce: 0,
+            data: undefined,
+            feeLimit: 0.2,
+          },
+        ],
+        VERIFIER_WALLET.publicKey,
+        VERIFIER_WALLET.proofOfAuth,
+      ),
+    ).toEqual(
+      {
+        ...BLOCK_1,
+        transactions: [
+          FROM_ROOT_TO_USER_1,
+        ],
+        lookup: {
+          [FROM_ROOT_TO_USER_1.to]: {
+            balance: FROM_ROOT_TO_USER_1.value,
+          },
+          [FROM_ROOT_TO_USER_1.from]: {
+            nonce: 0,
+            balance: TOTAL_COIN_SUPPLY
+                - FROM_ROOT_TO_USER_1.value
+                - (BASE_TX_FEE * 1),
+            fees: [
+              BASE_TX_FEE,
+            ],
+          },
+        },
+      },
+    );
+  });
+
+  it('handles txs from multiple from addresses', () => {
+    createBlockIdSpy.mockReturnValueOnce(id);
+    nowSpy.mockReturnValueOnce(timestamp);
+    const user1ToUser2 = {
+      from: USER_WALLET_1.publicKey,
+      to: USER_WALLET_2.publicKey,
+      value: 5,
+      nonce: 0,
+      data: undefined,
+      feeLimit: 0.2,
+    };
+    expect(
+      txPoolToBlock(
+        createLedger(),
+        [
+          FROM_ROOT_TO_USER_1,
+          user1ToUser2,
+        ],
+        VERIFIER_WALLET.publicKey,
+        VERIFIER_WALLET.proofOfAuth,
+      ),
+    ).toEqual(
+      {
+        ...BLOCK_1,
+        transactions: [
+          FROM_ROOT_TO_USER_1,
+          user1ToUser2,
+        ],
+        lookup: {
+          [FROM_ROOT_TO_USER_1.to]: {
+            balance: FROM_ROOT_TO_USER_1.value - user1ToUser2.value - BASE_TX_FEE,
+            nonce: 0,
+            fees: [
+              BASE_TX_FEE,
+            ],
+          },
+          [user1ToUser2.to]: {
+            balance: user1ToUser2.value,
+          },
+          [FROM_ROOT_TO_USER_1.from]: {
+            nonce: 0,
+            balance: TOTAL_COIN_SUPPLY
+                - FROM_ROOT_TO_USER_1.value
+                - (BASE_TX_FEE * 1),
+            fees: [
+              BASE_TX_FEE,
+            ],
+          },
+        },
       },
     );
   });
--- packages/validator/lib/index.js
+++ packages/validator/lib/index.js
@@ -14,42 +14,56 @@ const txPoolToBlock = (ledger, pool, verifier, proofOfAuth) => {
   if (!Array.isArray(pool)) {
     throw new Error('Invalid tx pool.');
   }
+  const prevLookup = {};
   const lookup = {};
+  const currentOrPrevLookup = (addr) => (lookup[addr] ? lookup[addr] : prevLookup[addr]);
   const txs = [];
 
-  pool
-    .sort((a, b) => a.nonce - b.nonce)
-    .forEach((tx) => {
-      let fromItem = lookup[tx.from];
-      let toItem = lookup[tx.to];
-      if (!fromItem) {
-        fromItem = {
-          nonce: findLatestLookupFieldValue(ledger, tx.from, 'nonce') || 0,
-          balance: findLatestLookupFieldValue(ledger, tx.from, 'balance') || 0,
-          fees: [],
-        };
-      }
-      if (tx.to && !toItem) {
-        toItem = {
-          balance: findLatestLookupFieldValue(ledger, tx.to, 'balance') || 0,
-        };
-      }
-      if (fromItem.balance >= tx.value + tx.feeLimit) {
-        const fee = calculateFee(tx);
-        fromItem.balance -= tx.value + fee;
-        fromItem.fees.push(fee);
-        fromItem.nonce = tx.nonce;
-        if (toItem) {
-          toItem.balance += tx.value;
-        }
-        txs.push(tx);
-      }
+  const sortedPool = pool.sort((a, b) => a.nonce - b.nonce);
 
+  const nonceFrom = {};
+  sortedPool.filter((tx) => {
+    const key = `${tx.from}${tx.nonce}`;
+    if (nonceFrom[key]) {
+      return false;
+    }
+    nonceFrom[key] = true;
+    return true;
+  }).forEach((tx) => {
+    let fromItem = currentOrPrevLookup(tx.from);
+    let toItem = currentOrPrevLookup(tx.to);
+    if (!fromItem) {
+      fromItem = {
+        balance: findLatestLookupFieldValue(ledger, tx.from, 'balance') || 0,
+        fees: [],
+      };
+      prevLookup[tx.from] = fromItem;
+    }
+    if (tx.to && !toItem) {
+      toItem = {
+        balance: findLatestLookupFieldValue(ledger, tx.to, 'balance') || 0,
+      };
+      prevLookup[tx.to] = toItem;
+    }
+    if (
+      fromItem.balance >= tx.value + tx.feeLimit
+    ) {
+      const fee = calculateFee(tx);
+      fromItem.balance -= tx.value + fee;
+      if (!fromItem.fees) {
+        fromItem.fees = [fee];
+      } else {
+        fromItem.fees.push(fee);
+      }
+      fromItem.nonce = tx.nonce;
       lookup[tx.from] = fromItem;
       if (toItem) {
+        toItem.balance += tx.value;
         lookup[tx.to] = toItem;
       }
-    });
+      txs.push(tx);
+    }
+  });
 
   return {
     prev: ledger.latestId,
